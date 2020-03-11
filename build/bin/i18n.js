802ae706cb76b73309ba35cfe8a6c800adb8ef14
starlee <1326646676@qq.com>
2020-03-11 14:18:50 +0800

    添加 eslint prettier  配置 (#10)
    * 更新源库代码 (#1)
    
    * 添加测试 make 命令 (#4)
    
    * modify gitignore and add package.json
    
    * feat: 添加 git  commitzen  githook lint-staged standard-version
    
    * feat: 添加 git  commitzen  githook lint-staged standard-version
    
    * feat: 添加 git  commitzen  githook lint-staged standard-version
    
    * feat: 添加 make new 测试命令
    
    Co-authored-by: Star (Shun) Li <Starlee.Li@fatri.cn>
    Co-authored-by: FatriFE <61823932+FatriFE@users.noreply.github.com>
    
    * webpack-dev-server (#5)
    
    * modify gitignore and add package.json
    
    * feat: 添加 git  commitzen  githook lint-staged standard-version
    
    * feat: 添加 git  commitzen  githook lint-staged standard-version
    
    * feat: 添加 git  commitzen  githook lint-staged standard-version
    
    * feat: 添加 make new 测试命令
    
    * feat: 添加 webpack 对 examples 文档的基础配置
    
    Co-authored-by: Star (Shun) Li <Starlee.Li@fatri.cn>
    Co-authored-by: FatriFE <61823932+FatriFE@users.noreply.github.com>
    
    * feat: 搭建文档系统
    
    * 完成文档系统架构 (#6)
    
    * modify gitignore and add package.json
    
    * feat: 添加 git  commitzen  githook lint-staged standard-version
    
    * feat: 添加 git  commitzen  githook lint-staged standard-version
    
    * feat: 添加 git  commitzen  githook lint-staged standard-version
    
    * feat: 添加 make new 测试命令
    
    * feat: 添加 webpack 对 examples 文档的基础配置
    
    * feat: 搭建文档系统
    
    * feat: 更改小logo图片
    
    * chore(release): 1.1.0
    
    * feat: 日志更新
    
    Co-authored-by: Star (Shun) Li <Starlee.Li@fatri.cn>
    Co-authored-by: FatriFE <61823932+FatriFE@users.noreply.github.com>
    
    * fix: 更改日志页面现实的日志文件名称
    
    * fix: 更改readme 中 fork 说明
    
    Co-authored-by: Star (Shun) Li <Starlee.Li@fatri.cn>
    Co-authored-by: FatriFE <61823932+FatriFE@users.noreply.github.com>
    
    * feat: add eslint and prettier config
    
    Co-authored-by: Star (Shun) Li <Starlee.Li@fatri.cn>
    Co-authored-by: FatriFE <61823932+FatriFE@users.noreply.github.com>

--- build/bin/i18n.js
+++ build/bin/i18n.js
@@ -1,24 +1,24 @@
-const fs = require('fs');
-const path = require('path');
-const langConfig = require('../../examples/i18n/page.json');
+const fs = require('fs')
+const path = require('path')
+const langConfig = require('../../examples/i18n/page.json')
 
 langConfig.forEach(lang => {
-    try {
-        fs.statSync(path.resolve(__dirname, `../../examples/pages/${ lang.lang }`));
-    } catch (e) {
-        fs.mkdirSync(path.resolve(__dirname, `../../examples/pages/${ lang.lang }`));
-    }
+  try {
+    fs.statSync(path.resolve(__dirname, `../../examples/pages/${lang.lang}`))
+  } catch (e) {
+    fs.mkdirSync(path.resolve(__dirname, `../../examples/pages/${lang.lang}`))
+  }
 
-    Object.keys(lang.pages).forEach(page => {
-        const templatePath = path.resolve(__dirname, `../../examples/pages/template/${ page }.tpl`);
-        const outputPath = path.resolve(__dirname, `../../examples/pages/${ lang.lang }/${ page }.vue`);
-        let content = fs.readFileSync(templatePath, 'utf8');
-        const pairs = lang.pages[page];
+  Object.keys(lang.pages).forEach(page => {
+    const templatePath = path.resolve(__dirname, `../../examples/pages/template/${page}.tpl`)
+    const outputPath = path.resolve(__dirname, `../../examples/pages/${lang.lang}/${page}.vue`)
+    let content = fs.readFileSync(templatePath, 'utf8')
+    const pairs = lang.pages[page]
 
-        Object.keys(pairs).forEach(key => {
-            content = content.replace(new RegExp(`<%=\\s*${ key }\\s*>`, 'g'), pairs[key]);
-        });
+    Object.keys(pairs).forEach(key => {
+      content = content.replace(new RegExp(`<%=\\s*${key}\\s*>`, 'g'), pairs[key])
+    })
 
-        fs.writeFileSync(outputPath, content);
-    });
-});
+    fs.writeFileSync(outputPath, content)
+  })
+})
--- packages/row/index.js
+++
@@ -1,5 +0,0 @@
-import Row from './src/main';
-Row.install = (Vue) => {
-    Vue.component(Row.name, Row)
-}
-export default Row
