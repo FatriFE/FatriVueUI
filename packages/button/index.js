8df3813787cab206222b7df8b2ffcdad24ed2daa
starlee <1326646676@qq.com>
2020-03-16 14:26:09 +0800

    添加 eslint prettier 逻辑 (#22)
    * feat: 添加打包配置
    
    * feat: 添加文档打包逻辑
    
    * fix: change travis
    
    * feat: add deploy:web config
    
    * fix: change travis config
    
    * fix: test travis
    
    * fix: change travis
    
    * 单选/多选下拉框 (#15) (#16)
    
    * feat: 新增电话号码输入框
    
    * feat: 添加特殊下拉框
    
    Co-authored-by: L_linc <37829161+L-linc@users.noreply.github.com>
    
    * feat: add switch component
    
    * fix: 更改项目名称
    
    * switch 组件 (#18) (#4)
    
    * feat: 添加打包配置
    
    * feat: 添加文档打包逻辑
    
    * fix: change travis
    
    * feat: add deploy:web config
    
    * fix: change travis config
    
    * fix: test travis
    
    * fix: change travis
    
    * 单选/多选下拉框 (#15) (#16)
    
    * feat: 新增电话号码输入框
    
    * feat: 添加特殊下拉框
    
    Co-authored-by: L_linc <37829161+L-linc@users.noreply.github.com>
    
    * feat: add switch component
    
    Co-authored-by: Star (Shun) Li <Starlee.Li@fatri.cn>
    Co-authored-by: fatrife <fatrife@aliyun.com>
    Co-authored-by: FatriFE <61823932+FatriFE@users.noreply.github.com>
    Co-authored-by: L_linc <37829161+L-linc@users.noreply.github.com>
    
    Co-authored-by: Star (Shun) Li <Starlee.Li@fatri.cn>
    Co-authored-by: fatrife <fatrife@aliyun.com>
    Co-authored-by: FatriFE <61823932+FatriFE@users.noreply.github.com>
    Co-authored-by: L_linc <37829161+L-linc@users.noreply.github.com>
    
    * fix: 更改文档中项目名称
    
    * feat: 更新umd/commonent/commonJS webpack 配置 && 添加switch 组件方法 && 更新switch文档
    
    * chore(release): 1.2.0
    
    * feat: 更新版本
    
    * feat: 添加项目prettier 代码风格检查
    
    * test: test eslint when pro env
    
    * fix: 更改 build-entry
    
    * fix: 更改 lint-staged
    
    * test: test
    
    * fix: test 2
    
    Co-authored-by: Star (Shun) Li <Starlee.Li@fatri.cn>
    Co-authored-by: fatrife <fatrife@aliyun.com>
    Co-authored-by: FatriFE <61823932+FatriFE@users.noreply.github.com>
    Co-authored-by: L_linc <37829161+L-linc@users.noreply.github.com>

--- packages/button/index.js
+++ packages/button/index.js
@@ -1,5 +1,5 @@
-import Button from './src/main';
-Button.install = (Vue) => {
-    Vue.component(Button.name, Button)
+import Button from './src/main'
+Button.install = Vue => {
+  Vue.component(Button.name, Button)
 }
 export default Button
