14c261b6f92463af4e797249dc0faabd70b20d8c
fatrife <fatrife@aliyun.com>
2020-05-26 14:16:52 +0800

    feat: change eslintignore

---
+++ packages/icon/index.js
@@ -0,0 +1,5 @@
+import Icon from './src/main'
+Icon.install = Vue => {
+  Vue.component(Icon.name, Icon)
+}
+export default Icon
---
+++ packages/icon/src/main.vue
@@ -0,0 +1,17 @@
+<template>
+  <i class="fa" :class="`fa-${name}`" aria-hidden="true" />
+</template>
+
+<script>
+import '../font-awesome-4.7.0/css/font-awesome.min.css'
+export default {
+  name: 'FaIcon',
+  props: {
+    name: {
+      type: String,
+      required: false,
+      default: 'font-awesome'
+    }
+  }
+}
+</script>
